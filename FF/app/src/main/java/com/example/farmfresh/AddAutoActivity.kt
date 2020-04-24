6366e29b5b570c1113ad9d22b8ac93237fb30283
aniketbote2098 <aniketbote2098@gmail.com>
2020-04-24 17:59:39 +0530

    Minor Changes

--- FF/app/src/main/java/com/example/farmfresh/AddAutoActivity.kt
+++ FF/app/src/main/java/com/example/farmfresh/AddAutoActivity.kt
@@ -1,8 +1,10 @@
 package com.example.farmfresh
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.common.api.Status
 import com.google.android.libraries.places.api.Places
@@ -10,42 +12,53 @@ import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.api.net.PlacesClient
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import kotlinx.android.synthetic.main.activity_addauto.*
 import java.util.*
 
-class AutoFillActivity : AppCompatActivity(){
+class AddAutoActivity : AppCompatActivity(){
     lateinit var placesClient: PlacesClient
     var placeFields = Arrays.asList(
         Place.Field.ID,
         Place.Field.NAME,
         Place.Field.ADDRESS)
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_autofill)
-        Log.d("AutofillActivity","Inside AutoFill")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_addauto)
 
         initPlaces()
-        setuoPlacesAutocomplete()
+        setupPlacesAutocomplete()
 
     }
+
     private fun initPlaces(){
+        Log.d("AddAutoActivity","Function : initplaces")
         Places.initialize(this,getString(R.string.api_key))
         placesClient = Places.createClient(this)
         Log.d("AutofillActivity","Inside AutoFill")
     }
-    private  fun setuoPlacesAutocomplete(){
+
+    private  fun setupPlacesAutocomplete(){
+        Log.d("AddAutoActivity","Function : setupplaces")
         val autocompleteFragment = supportFragmentManager
             .findFragmentById(R.id.fragment_autofill) as AutocompleteSupportFragment
         autocompleteFragment.setPlaceFields(placeFields)
         autocompleteFragment.setOnPlaceSelectedListener(object: PlaceSelectionListener {
             override fun onPlaceSelected(p0: Place) {
-                Log.d("Registeractivity","Success : $p0")
+                Log.d("AddAutoActivity","Address selected succesfully")
+                val returnIntent:Intent = Intent()
+                returnIntent.putExtra("address","${p0.address}")
+                setResult(Activity.RESULT_OK, returnIntent)
+                finish()
             }
 
             override fun onError(p0: Status) {
-                Log.d("Registeractivity","Error : $p0")
+                Log.d("AutoFillActivity","Error occured during address select")
             }
 
-        } )
+        })
     }
+
+
 }
