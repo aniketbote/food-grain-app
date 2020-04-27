90df224d0edbf990d39f158601b9a3fddf522ff3
aniketbote2098 <aniketbote2098@gmail.com>
2020-04-27 13:58:20 +0530

    Bug Fixes

--- FF/app/src/main/java/com/example/farmfresh/IndexActivity.kt
+++ FF/app/src/main/java/com/example/farmfresh/IndexActivity.kt
@@ -16,6 +16,7 @@ import androidx.core.view.GravityCompat
 import androidx.fragment.app.FragmentTransaction
 import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.database.snapshot.Index
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.activity_index.*
 import kotlinx.android.synthetic.main.activity_register.*
@@ -26,11 +27,6 @@ import kotlinx.android.synthetic.main.header_nav.*
 class IndexActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener{
 
     lateinit var homeFragment: HomeFragment
-    lateinit var currentOrdersFragment: CurrentOrdersFragment
-    lateinit var previousOrdersFragment: PreviousOrdersFragment
-    lateinit var supportFragment: SupportFragment
-    lateinit var profileFragment: ProfileFragment
-    lateinit var logOutFragment: LogOutFragment
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,16 +37,24 @@ class IndexActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelecte
         val name = token.getString("name","")
         val photoUrl = token.getString("imageUri","").toString()
         Log.d("IndexActivity","Name = ${name}")
-        Log.d("IndexActivity","Name = ${photoUrl}")
+        Log.d("IndexActivity","PhotoUrl = ${photoUrl}")
 
 
         val nv:NavigationView = findViewById(R.id.nav_activity_index)
         val navView:View = nv.getHeaderView(0)
         val tv:TextView = navView.findViewById(R.id.name_header_nav)
         tv.setText("$name")
+        Log.d("IndexActivity","Name Set on Nav Bar")
 
         val imageView:CircleImageView = navView.findViewById(R.id.showPhoto_header_nav)
         Glide.with(this).load("${photoUrl}").into(imageView)
+        Log.d("IndexActivity","Image Loaded On Nav Bar")
+
+        tv.setOnClickListener {
+            Log.d("IndexActivity","Pressed Profile Button : ${name}")
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
 
 
 
@@ -85,7 +89,7 @@ class IndexActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelecte
             .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             .commit()
 
-        }
+    }
 
     override fun onNavigationItemSelected(MenuItem: MenuItem): Boolean {
 
@@ -93,49 +97,26 @@ class IndexActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelecte
         {
 
             R.id.home->{
-                homeFragment = HomeFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout,homeFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
+                Log.d("IndexActivity","Pressed Home Button")
+                val intent = Intent(this, IndexActivity::class.java)
+                startActivity(intent)
             }
             R.id.current_order -> {
-                currentOrdersFragment = CurrentOrdersFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout,currentOrdersFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-
+                Log.d("IndexActivity","Pressed Current Orders")
+                val intent = Intent(this, CurrentOrdersActivity::class.java)
+                startActivity(intent)
             }
-            R.id.current_order -> {
-                previousOrdersFragment = PreviousOrdersFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout,previousOrdersFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-
+            R.id.previous_orders -> {
+                Log.d("IndexActivity","Pressed Previous Orders")
+                val intent = Intent(this, PreviousOrdersActivity::class.java)
+                startActivity(intent)
             }
             R.id.support -> {
-                supportFragment = SupportFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout,supportFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
-
+                Log.d("IndexActivity","Pressed Support")
+                val intent = Intent(this, SupportActivity::class.java)
+                startActivity(intent)
             }
-            R.id.myprofile -> {
-                profileFragment = ProfileFragment()
-                supportFragmentManager
-                    .beginTransaction()
-                    .replace(R.id.frame_layout,profileFragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                    .commit()
 
-            }
             R.id.logout -> {
                 Log.d("IndexActivity","Pressed Log Out")
                 val token = getSharedPreferences("UserSharedPreferences",Context.MODE_PRIVATE)
@@ -149,7 +130,7 @@ class IndexActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelecte
                 editor.putString("imageUri","")
                 editor.putString("birthdate","")
                 editor.commit()
-                Log.d("IndexActivity","User Info Deleted from Shared preferences ")
+                Log.d("IndexActivity","User Info Deleted from Shared preferences")
                 val intent = Intent(this,LoginActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(intent)
@@ -173,5 +154,4 @@ class IndexActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelecte
 
     }
 }
-
 
