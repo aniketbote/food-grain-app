ab786d17dbe00d1cca8a2cd2947c457783232b76
aniketbote2098 <aniketbote2098@gmail.com>
2020-04-28 15:23:07 +0530

    Profile page completed

--- FF/app/src/main/java/com/example/farmfresh/ProfileActivity.kt
+++ FF/app/src/main/java/com/example/farmfresh/ProfileActivity.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
 import com.google.android.material.navigation.NavigationView
 import de.hdodenhof.circleimageview.CircleImageView
@@ -24,8 +26,25 @@ class ProfileActivity : AppCompatActivity(){
         val birthdate = token.getString("birthdate","")
         Log.d("ProfileActivity","${imageUri}")
 
-        val imageView: CircleImageView = findViewById(R.id.userphoto_profile)
+        val constraintLayout: ConstraintLayout = findViewById(R.id.constraintLayout_profile)
+        val imageView: CircleImageView = constraintLayout.findViewById(R.id.userphoto_profile)
+
         Glide.with(this).load("${imageUri}").into(imageView)
+
+        val useraddress: TextView = constraintLayout.findViewById(R.id.useraddress_profile)
+        useraddress.text = address
+
+        val username: TextView = constraintLayout.findViewById(R.id.username_profile)
+        username.text = name
+
+        val useremail: TextView = constraintLayout.findViewById(R.id.useremail_profile)
+        useremail.text = email
+
+        val userphone: TextView = constraintLayout.findViewById(R.id.userphone_profile)
+        userphone.text = phone
+
+
+
         Log.d("IndexActivity","Image Loaded On Nav Bar")
     }
 }
