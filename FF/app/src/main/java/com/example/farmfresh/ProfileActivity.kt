be4ee11577857a44ec8a2740a4b625157edf713d
aniketbote2098 <aniketbote2098@gmail.com>
2020-04-28 15:08:43 +0530

    Minor changes

--- FF/app/src/main/java/com/example/farmfresh/ProfileActivity.kt
+++ FF/app/src/main/java/com/example/farmfresh/ProfileActivity.kt
@@ -2,7 +2,12 @@ package com.example.farmfresh
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.android.material.navigation.NavigationView
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ProfileActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +22,10 @@ class ProfileActivity : AppCompatActivity(){
         val gender = token.getString("gender","")
         val imageUri = token.getString("imageUri","")
         val birthdate = token.getString("birthdate","")
+        Log.d("ProfileActivity","${imageUri}")
+
+        val imageView: CircleImageView = findViewById(R.id.userphoto_profile)
+        Glide.with(this).load("${imageUri}").into(imageView)
+        Log.d("IndexActivity","Image Loaded On Nav Bar")
     }
 }
