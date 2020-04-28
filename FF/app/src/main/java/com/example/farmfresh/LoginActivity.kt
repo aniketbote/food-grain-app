cedae75910750a673701ae8991da1f0791606389
aniketbote2098 <aniketbote2098@gmail.com>
2020-04-28 17:53:54 +0530

    Removed data fetching from loginactivity

--- FF/app/src/main/java/com/example/farmfresh/LoginActivity.kt
+++ FF/app/src/main/java/com/example/farmfresh/LoginActivity.kt
@@ -18,55 +18,10 @@ import java.io.Serializable
 import java.security.MessageDigest
 
 class LoginActivity : AppCompatActivity(){
-    lateinit var featuredListObj : featureLabels
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val builder = AlertDialog.Builder(this)
-        val dialogView = layoutInflater.inflate(R.layout.progress_bar,null)
-        builder.setView(dialogView)
-        builder.setCancelable(false)
-        val message = dialogView.findViewById<TextView>(R.id.text_progressBar)
-        message.text = "Logging In"
-        val dialog = builder.create()
-
-        val token = getSharedPreferences("UserSharedPreferences", Context.MODE_PRIVATE)
-        val emailHash = token.getString("EMAILHASH","")
-
-        if ( emailHash!= "" ){
-            dialog.show()
-        }
-
-        Log.d("LoginActivity","Fetching Featured Images from database")
-        val ref = FirebaseDatabase.getInstance().getReference("/featured")
-        ref.addValueEventListener(object: ValueEventListener{
-            override fun onCancelled(p0: DatabaseError) {
-                Log.d("LoginActivity","Error in Fetching Featured Images : ${p0}")
-                return
-            }
-
-            override fun onDataChange(p0: DataSnapshot) {
-                val featureList = mutableListOf<String>()
-                Log.d("LoginActivity","Successfully Fetched Featured Images ")
-                for (name in p0.children){
-                    Log.d("LoginActivity","Image Location : ${name.value.toString()}")
-                    featureList.add(name.value.toString())
-                }
-
-                featuredListObj = featureLabels(featureList)
-                if ( emailHash!= "" ){
-                    Log.d("LoginActivity","User Already Logged In :${emailHash}")
-                    val indexIntent = Intent(this@LoginActivity, IndexActivity::class.java)
-                    indexIntent.putExtra("featuredListObj", featuredListObj)
-                    Log.d("LoginActivity","User Logged In : Starting IndexActivity")
-                    startActivity(indexIntent)
-                    dialog.dismiss()
-                    finish()
-                }
-            }
-
-        })
         registration_login.setOnClickListener {
             Log.d("LoginActivity","Clicked Register Button")
             val registerIntent = Intent(this, RegisterActivity::class.java)
@@ -126,9 +81,7 @@ class LoginActivity : AppCompatActivity(){
                         val phone = p0.child("phone").value.toString()
                         val address = p0.child("address").value.toString()
                         val name = p0.child("name").value.toString()
-                        val gender = p0.child("gender").value.toString()
                         val imageUri = p0.child("imageUri").value.toString()
-                        val birthdate = p0.child("birthdate").value.toString()
 
 
                         val pref = getSharedPreferences("UserSharedPreferences", Context.MODE_PRIVATE)
@@ -138,19 +91,15 @@ class LoginActivity : AppCompatActivity(){
                         editor.putString("phone",phone)
                         editor.putString("address",address)
                         editor.putString("name",name)
-                        editor.putString("gender",gender)
                         editor.putString("imageUri",imageUri)
-                        editor.putString("birthdate",birthdate)
-                        editor.putString("user","created")
                         editor.commit()
                         Log.d("LoginActivity","User Info hash stored in Shared preferences")
 
-                        val indexIntent:Intent = Intent(this@LoginActivity, IndexActivity::class.java)
-                        indexIntent.putExtra("featuredListObj",featuredListObj)
-                        Log.d("LoginActivity","Starting IndexActivity")
+                        val loadingIntent:Intent = Intent(this@LoginActivity, LoadingActivity::class.java)
+                        Log.d("LoginActivity","Starting LoadingActivity")
                         Toast.makeText(this@LoginActivity,"Login Successful",Toast.LENGTH_SHORT).show()
-                        indexIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        startActivity(indexIntent)
+                        loadingIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(loadingIntent)
                         dialog.dismiss()
                         finish()
                     }
@@ -172,4 +121,3 @@ class LoginActivity : AppCompatActivity(){
 
 }
 
-class featureLabels(val featureList: List<String>) : Serializable
