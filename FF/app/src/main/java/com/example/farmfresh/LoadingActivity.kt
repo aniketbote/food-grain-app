d6ac019357fff2855ae58591bc5e93fbcf6632ca
aniketbote2098 <aniketbote2098@gmail.com>
2020-04-30 14:10:15 +0530

    Added data fetch for all required data

--- FF/app/src/main/java/com/example/farmfresh/LoadingActivity.kt
+++ FF/app/src/main/java/com/example/farmfresh/LoadingActivity.kt
@@ -12,51 +12,69 @@ import com.google.firebase.database.ValueEventListener
 import java.io.Serializable
 
 
-class LoadingActivity : AppCompatActivity(){
-    lateinit var featuredListObj : featureLabels
+class LoadingActivity : AppCompatActivity() {
+    private val categoryList:List<String> = listOf<String>("Exotic_Fruits","Exotic_Vegetables","Foodgrains","Fruits","Vegetables")
+//    private val itemSubList:List<String> = listOf<String>("Available Quantity","Description","Image","Price","Size")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.loading_page)
 
         val token = getSharedPreferences("UserSharedPreferences", Context.MODE_PRIVATE)
-        val emailHash = token.getString("EMAILHASH","")
+        val emailHash = token.getString("EMAILHASH", "")
 
-        if(emailHash == ""){
-            Log.d("LoadingActivity","Starting LoginActivity")
-            val loginIntent = Intent(this,LoginActivity::class.java)
+        if (emailHash == "") {
+            Log.d("LoadingActivity", "Starting LoginActivity")
+            val loginIntent = Intent(this, LoginActivity::class.java)
             startActivity(loginIntent)
             finish()
         }
 
-        if ( emailHash!= "" ){
-        Log.d("LoadingActivity","Fetching Featured Images from database")
-        val ref = FirebaseDatabase.getInstance().getReference("/featured")
-        ref.addValueEventListener(object: ValueEventListener {
-            override fun onCancelled(p0: DatabaseError) {
-                Log.d("LoadingActivity","Error in Fetching Featured Images : ${p0}")
-                return
-            }
-
-            override fun onDataChange(p0: DataSnapshot) {
-                val featureList = mutableListOf<String>()
-                Log.d("LoadingActivity","Successfully Fetched Featured Images ")
-                for (name in p0.children){
-                    Log.d("LoadingActivity","Image Location : ${name.value.toString()}")
-                    featureList.add(name.value.toString())
+        if (emailHash != "") {
+            Log.d("LoadingActivity", "Fetching data from database")
+            val refFeatured = FirebaseDatabase.getInstance().getReference("/all_items")
+            refFeatured.addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                    Log.d("LoadingActivity", "Error in Fetching Featured data : ${p0}")
+                    return
                 }
 
-                featuredListObj = featureLabels(featureList)
-                Log.d("LoadingActivity","User Already Logged In :${emailHash}")
-                val indexIntent = Intent(this@LoadingActivity, IndexActivity::class.java)
-                indexIntent.putExtra("featuredListObj", featuredListObj)
-                Log.d("LoadingActivity","User Logged In : Starting IndexActivity")
-                startActivity(indexIntent)
-                finish()
-            }
-        })
+                override fun onDataChange(p0: DataSnapshot) {
+                    val featureList = mutableListOf<String>()
+                    Log.d("LoadingActivity", "Successfully Fetched data")
+                    for (name in p0.child("featured").children) {
+//                        Log.d("LoadingActivity", "Image Location : ${name.value.toString()}")
+                        featureList.add(name.value.toString())
+                    }
+                    Log.d("LoadingActivity","FeatureList Created")
+
+                    val finalHash = HashMap<String, List<HashMap<String,String>>>()
+                    for (name in categoryList){
+                        val tempList = mutableListOf<HashMap<String,String>>()
+                        for(itemName in p0.child(name).children){
+                            val tempHashMap = HashMap<String,String>()
+                            tempHashMap.put("Name",itemName.key.toString())
+                            for (subItem in itemName.children){
+                                tempHashMap.put(subItem.key.toString(), subItem.value.toString())
+                            }
+                            tempList.add(tempHashMap)
+                        }
+                        Log.d("LoadingActivity","${name} added in finalHash ")
+                        finalHash.put(name,tempList)
+                    }
+                    Log.d("LoadingActivity","finalHash Created: Creating dataObj")
+                    val dataObj = initData(featureList, finalHash)
+//                    Log.d("LoadingActivity","${dataObj.featureList}")
+                    val indexIntent = Intent(this@LoadingActivity, IndexActivity::class.java)
+                    indexIntent.putExtra("dataObj", dataObj)
+                    Log.d("LoadingActivity", "User Logged In : Starting IndexActivity")
+                    startActivity(indexIntent)
+                    finish()
+                }
+            })
         }
     }
 }
 
 
-class featureLabels(val featureList: List<String>) : Serializable
+class initData(val featureList: List<String>, val finalHash: HashMap<String, List<HashMap<String,String>>>) : Serializable
